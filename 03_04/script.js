ee769878a23da90eb9b5f35ca5638f3c212c1dbe
Swaroop Devaraju <swaroopdevaraju1@gmail.com>
2021-01-23 02:37:33 -0500

    string and number operations in JavaScript

---
+++ 03_04/script.js
@@ -0,0 +1,10 @@
+var a = 5;
+var b = "4";
+var sum = a + b; // number is converted to string.
+var product = a * b // assumes that b is a number
+var diff = a - b // same
+var quotiont = a / b // same
+
+var c = "brah";
+var sum2 = a + c; // concatinates a and c
+var product2 = a * c // should say NaN (not a number)
