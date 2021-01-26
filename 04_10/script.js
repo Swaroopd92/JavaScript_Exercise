dcebcf4670850334455b86d9e2a1eeee1b8eaae9
Swaroop Devaraju <swaroopdevaraju1@gmail.com>
2021-01-25 21:11:20 -0500

    Object constructors in JavaScript.

---
+++ 04_10/script.js
@@ -0,0 +1,26 @@
+function Course(title, instructor, level, published, views) {
+    this.title = title;
+    this.instructor = instructor;
+    this.level = level;
+    this.published = published;
+    this.views = views;
+
+    this.updateViews = function() {
+        return ++this.views;
+    };
+}
+
+
+
+var course01 = new Course("JavaScript training", "FName LName", 1, true, 0);
+var course02 = new Course("Up and running", "Swaroop D", 1, true, 100);
+
+var courses = [
+    new Course("Up and runninggggg", "Swaroop Deee", 1, true, 101),
+    new Course("Up and runningota", "Swaroop Dota", 1, true, 102)
+];
+
+console.log(course01);
+console.log(course02);
+
+console.log(courses);
