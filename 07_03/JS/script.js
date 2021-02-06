7f04a43231b0c2c3f0d5c0cb253ed153243dd9ea
Swaroop Devaraju <swaroopdevaraju1@gmail.com>
2021-02-05 20:10:09 -0500

    Triggering functions with event handlers.

---
+++ 07_03/JS/script.js
@@ -0,0 +1,18 @@
+const CTA = document.querySelector(".cta a");
+const ALERT = document.querySelector("#booking-alert");
+
+CTA.classList.remove("hide");
+ALERT.classList.add("hide");
+
+/**
+ * @param {Event} e
+ */
+
+function reveal(e) {
+    e.preventDefault()
+    CTA.classList.toggle("hide");
+    ALERT.classList.toggle("hide");
+}
+
+CTA.onclick = reveal;
+
