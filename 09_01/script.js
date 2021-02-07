cbbc6af2ef3b97d3b0a8a9795ccb6be96b748005
Swaroop Devaraju <swaroopdevaraju1@gmail.com>
2021-02-06 23:39:19 -0500

    loops in JS.. smh :|:/:?

---
+++ 09_01/script.js
@@ -0,0 +1,17 @@
+// for loop
+
+for (var i = 0; i < 10; ++i) {
+    console.log(i);
+}
+
+//while loop
+while (i < 20) {
+    console.log(i);
+    i++;
+}
+
+//do while loop
+do {
+    console.log(i);
+    i++;
+} while(i < 30);
